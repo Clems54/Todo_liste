70897c244ce6bd68dbd67b337d6b5d72f7bf1c84
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-12 11:32:12 +0100

    fin de création classe product

--- app/src/main/java/fr/univ/clement/todo_liste/Product.java
+++ app/src/main/java/fr/univ/clement/todo_liste/Product.java
@@ -4,12 +4,28 @@ package fr.univ.clement.todo_liste;
  * Created by cleme on 12/02/2018.
  */
 
-public class Product {
+public class Product{
     private String title;
     private String category;
 
-    public Product(String title, String category) {
+    public Product(String title, String category){
         this.title = title;
+        this.category = category;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    public void setCategory(String category){
         this.category = category;
     }
 }
