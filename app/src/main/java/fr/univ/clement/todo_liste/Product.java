f6a0d646fd8328c207c55ab099f88d3aca50fd73
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-12 11:29:03 +0100

    ajout de la classe d'item

---
+++ app/src/main/java/fr/univ/clement/todo_liste/Product.java
@@ -0,0 +1,15 @@
+package fr.univ.clement.todo_liste;
+
+/**
+ * Created by cleme on 12/02/2018.
+ */
+
+public class Product {
+    private String title;
+    private String category;
+
+    public Product(String title, String category) {
+        this.title = title;
+        this.category = category;
+    }
+}
