b947498c61d440464dad8616edc621951e798144
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-16 11:31:39 +0100

    Suppression import inutile

--- app/src/main/java/fr/univ/clement/todo_liste/MainActivity.java
+++ app/src/main/java/fr/univ/clement/todo_liste/MainActivity.java
@@ -34,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         recycler.setItemAnimator(new DefaultItemAnimator());
         recycler.setAdapter(itemAdapter);
-
-
     }
 
     @Override
